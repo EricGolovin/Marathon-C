fdf824ff89e670c173d0fbcf1002e0609a967b75
egolovin <ericgolovin@outlook.com>
2019-11-22 10:10:58 +0200

    added sprint08 t00

---
+++ marathon/sprint08/t00/main.c
@@ -0,0 +1,6 @@
+#include "header.h"
+#include <stdio.h>
+int main() {
+	mx_printstr("hello");
+	return 0;
+}
---
+++ marathon/sprint08/t00/mx_isdigit.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+bool mx_isdigit(int c) {
+	for (int i = '0'; i <= '9'; ++i) {
+		if (c == i) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
